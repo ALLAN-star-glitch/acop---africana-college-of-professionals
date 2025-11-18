728f81979736d89639625b2e90440dc13e6027e9
Allan M <admin@EBLMICTHTGXHVWVXW.local>
2025-11-18 22:06:48 +0300

    updated admissions to admission

--- lib/constants/MenuItems.ts
+++ lib/constants/MenuItems.ts
@@ -3,7 +3,7 @@ export const desktopMenuItems = [
   { label: 'Home', href: '/' },
   { label: 'About', href: '/about' },
   { label: 'Courses', href: '/courses' },
-  { label: 'Admissions', href: '/admissions' },
+  { label: 'Admission', href: '/admission' },
   { label: 'Masomo Portal', href: 'https://moodle-200882-0.cloudclusters.net/', external: true },
   { label: 'Contact', href: 'https://form.jotform.com/253172041859559', external: true },
 ]
